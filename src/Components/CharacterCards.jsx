2c503b4b46277192aa4aca334bef6dc55ced5cdb
SamuelFDEZS <thggsamuel@gmail.com>
2026-09-16 14:39:17 +0200

    Refactor CharacterCards.jsx: mejorar la legibilidad y reubicar la función de obtención de datos

--- src/Components/CharacterCards.jsx
+++ src/Components/CharacterCards.jsx
@@ -1,26 +1,26 @@
-import { useEffect} from "react"
+import { useEffect } from "react"
 const CharacterCards = ({ characters, setCharacters, filter }) => {
 
-    const get = async () => {
-        const response = await fetch("https://nintendo-node-js.vercel.app/mainCharacters");
-        const res = await response.json();
-        setCharacters(res.filter((character) => character.nombre.toLowerCase().includes(filter.toLowerCase())
-        ))
-    }
-
-    console.log(characters)
     useEffect(() => {
+        const get = async () => {
+            const response = await fetch("https://nintendo-node-js.vercel.app/mainCharacters");
+            const res = await response.json();
+            setCharacters(res.filter((character) => character.nombre.toLowerCase().includes(filter.toLowerCase())
+            ))
+        }
+
         get();
+
     }, [filter])
     return characters.map((character, index) => {
         return (
-                <article key={index} className="character-card">
-                    <img src={character.imagen} alt="Imagen del personaje" />
-                    <div className="character-information">
-                        <h1>{character.nombre}</h1>
-                        <p className="description">{character.descripcion}</p>
-                    </div>
-                </article>
+            <article key={index} className="character-card">
+                <img src={character.imagen} alt="Imagen del personaje" />
+                <div className="character-information">
+                    <h1>{character.nombre}</h1>
+                    <p className="description">{character.descripcion}</p>
+                </div>
+            </article>
         )
     })
 }
